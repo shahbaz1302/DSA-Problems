a3329fdf1bb8013b34e4e983f8fbd4675ed828fa
Mohd Shahbaz Khan <160204508+shahbaz1302@users.noreply.github.com>
2026-07-27 14:13:11 +0530

    Time: 1 ms (100%), Space: 49.1 MB (5.04%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,39 +1,26 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i=0,j=0,count=0;
         int n1=nums1.length,n2=nums2.length;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int low=0,high=n1;
         int n=n1+n2;
-        int element1=-1;
-        int element2=-1;
-        int index2=n/2;
-        int index1=index2-1;
-        while(i<n1 && j<n2){
-            if(nums1[i]<nums2[j]){
-                if(count==index1) element1=nums1[i];
-                if(count==index2) element2=nums1[i];
-                count++;
-                i++;
+        int left=(n+1)/2;
+        while(low<=high){
+            int mid1=low+(high-low)/2;
+            int mid2=left-mid1;
+            int l1=Integer.MIN_VALUE,l2=Integer.MIN_VALUE;
+            int r1=Integer.MAX_VALUE,r2=Integer.MAX_VALUE;
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1) return Math.max(l1,l2);
+                return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2.0;
             }
-            else{
-                if(count==index1) element1=nums2[j];
-                if(count==index2) element2=nums2[j];
-                count++;
-                j++;
-            }
+            else if(l1>r2) high=mid1-1;
+            else low=mid1+1;
         }
-        while(i<n1){
-            if(count==index1) element1=nums1[i];
-            if(count==index2) element2=nums1[i];
-            count++;
-            i++;
-        }
-        while(j<n2){
-            if(count==index1) element1=nums2[j];
-            if(count==index2) element2=nums2[j];
-            count++;
-            j++;
-        }
-        if(n%2==1) return element2;
-        return (double)(element1+element2) /2.0;
+        return 0;
     }
 }
