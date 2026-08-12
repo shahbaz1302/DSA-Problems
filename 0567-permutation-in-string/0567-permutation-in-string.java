d2d71bd305ee6fc9f7eb550d98d7c4a89feb46e1
Mohd Shahbaz Khan <160204508+shahbaz1302@users.noreply.github.com>
2026-08-12 11:19:37 +0530

    Time: 27 ms (24.44%), Space: 45 MB (28.79%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length()>s2.length()) return false;
-        Map<Character,Integer> map = new HashMap<>();
+        Map<Character,Integer> map=new HashMap();
         for(int i=0;i<s1.length();i++) map.put(s1.charAt(i),map.getOrDefault(s1.charAt(i),0)+1);
-        int left = 0,count=s1.length();
-        for(int right = 0;right<s2.length();right++){
-            char ch = s2.charAt(right);
-            int val = map.getOrDefault(ch,0);
-            if(val>0) count--;
-            map.put(ch,val-1);
+        int count=s1.length(),left=0;
+        for(int right=0;right<s2.length();right++){
+            char ch=s2.charAt(right);
+            if(map.getOrDefault(ch,0)>0) count--;
+            map.put(ch,map.getOrDefault(ch,0)-1);
             if(count==0) return true;
             if(right-left+1==s1.length()){
-                char ch1 = s2.charAt(left);
-                int leftVal = map.get(ch1);
-                if(leftVal>=0) count++;
-                map.put(ch1,leftVal+1);
+                char ch1=s2.charAt(left);
+                if(map.getOrDefault(ch1,0)>=0) count++;
+                map.put(ch1,map.getOrDefault(ch1,0)+1);
                 left++;
             }
         }
