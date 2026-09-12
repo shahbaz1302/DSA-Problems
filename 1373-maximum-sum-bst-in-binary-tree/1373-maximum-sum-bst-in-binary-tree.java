06da17189291675c66d799a456fe409844d22419
Mohd Shahbaz Khan <160204508+shahbaz1302@users.noreply.github.com>
2026-09-12 19:28:25 +0530

    Time: 6 ms (91.82%), Space: 67.3 MB (17.55%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -15,13 +15,11 @@
  */
 class NodeValue {
     int maxNode,minNode,maxSum;
-    boolean isBST;
 
-    public NodeValue(int minNode, int maxNode, int maxSum,boolean isBST) {
+    public NodeValue(int minNode, int maxNode, int maxSum) {
         this.maxNode = maxNode;
         this.minNode = minNode;
         this.maxSum = maxSum;
-        this.isBST=isBST;
     }
 }
 
@@ -36,15 +34,15 @@ class Solution {
     }
 
     private NodeValue maxSumBSTHelper(TreeNode root) {
-        if (root == null) return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE, 0,true);
+        if (root == null) return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE, 0);
         NodeValue left = maxSumBSTHelper(root.left);
         NodeValue right = maxSumBSTHelper(root.right);
-        if(left.isBST && right.isBST && left.maxNode<root.val && root.val < right.minNode){
+        if(left.maxNode<root.val && root.val < right.minNode){
             int currentSum= left.maxSum+right.maxSum+root.val;
             globalSum=Math.max(globalSum,currentSum);
             return new NodeValue(Math.min(root.val, left.minNode), Math.max(root.val, right.maxNode),
-                    currentSum,true);
+                    currentSum);
         }
-        return new NodeValue(Integer.MIN_VALUE, Integer.MAX_VALUE, 0,false);
+        return new NodeValue(Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
     }
 }
